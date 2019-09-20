aae6ea638221b9bbe25bc02cb5d673bb530dabca
Lukas Mautner <lukas.mauter@tum.de>
2019-09-20 15:08:03 +0200

    showcase challenging cubic function

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ import numpy as np
 
 def order_3_reg_1x1():
     def original_func(x):
-        a3 = np.array([[0.]])
-        a2 = np.array([[.5]])
-        a1 = np.array([[0.]])
-        a0 = np.array([[2.]])
+        a3 = np.array([[-1.73]])
+        a2 = np.array([[2.902]])
+        a1 = np.array([[20.34]])
+        a0 = np.array([[3.0001]])
 
         return a3 * np.power(x, 3) + a2 * np.power(x, 2) + a1 * x + a0
 
@@ -16,7 +16,7 @@ def order_3_reg_1x1():
 
     layer_dimensions = [(1, 45), (45, 45), (45, 1)]
 
-    regression_1x1(-5., 5., 50000, 0.0001, layer_dimensions, original_func, out_interpretation)
+    regression_1x1(-5., 5., 20000, 0.0001, layer_dimensions, original_func, out_interpretation)
 
 
 def cat_3_1x1():
