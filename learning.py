fixed a major flaw with the activation gradients

learning.py
@@ -27,7 +27,7 @@ def get_data(original_func, lb, ub, size):
 
 def get_nn_from_architecture(layer_dimensions, loss, loss_der, hidden_act, hidden_act_der, output_act, output_act_der, out_interpretation):
     weights = [np.random.uniform(-1., 1., layer_dimension[::-1]) for layer_dimension in layer_dimensions]  # dimension is reversed because a R^n -> R^m function needs a m x n matrix
-    biases = [np.random.uniform(-0.1, .1, (layer_dimension[1], 1)) for layer_dimension in layer_dimensions]
+    biases = [np.random.uniform(-0.1, 0.1, (layer_dimension[1], 1)) for layer_dimension in layer_dimensions]
 
     layers = [Layer(weights[i], biases[i], hidden_act, hidden_act_der) for i in range(len(layer_dimensions) - 1)]
     layers.append(Layer(weights[-1], biases[-1], output_act, output_act_der))
@@ -87,11 +87,11 @@ def test_regression_1x1(nn, func, t_size, d_lb, d_ub):
 
 def classification_1x1(lb, ub, d_size, learning_rate, layer_dimensions, original_func, out_interpretation):
     def hidden_act(x):
-        return np.array([[np.tanh(xv) for xv in xs] for xs in x])
+        return np.array([[max(0, xv) for xv in xs] for xs in x])
 
     def hidden_act_der(x):
         def der(x):
-            return 1. - np.power(np.tanh(x), 2)
+            return 1. if x > 0. else 0.
 
         return np.array([[der(xv) for xv in xs] for xs in x])
 

main.py
@@ -16,11 +16,11 @@ def order_3_reg_1x1():
 
     layer_dimensions = [(1, 45), (45, 45), (45, 1)]
 
-    regression_1x1(-5., 5., 100000, 0.0001, layer_dimensions, original_func, out_interpretation)
+    regression_1x1(-5., 5., 50000, 0.0001, layer_dimensions, original_func, out_interpretation)
 
 
 def cat_3_1x1():
-    categories = np.array([1, 2, 3])
+    categories = np.array([5, 10, 15])
 
     def original_func(x):
         [[x_inner]] = x
@@ -36,9 +36,9 @@ def cat_3_1x1():
         max_i = np.argmax(x)
         return categories[max_i]
 
-    layer_dimensions = [(1, 50), (50, 50), (50, 3)]
+    layer_dimensions = [(1, 2), (2, 2), (2, 4), (4, 3)]
 
-    classification_1x1(-1., 1., 10000, 0.01, layer_dimensions, original_func, out_interpretation)
+    classification_1x1(-0.3333, 1., 1000, 0.001, layer_dimensions, original_func, out_interpretation)
 
 
 order_3_reg_1x1()

neural_net.py
@@ -59,7 +59,8 @@ class NeuralNet:
                 def activation_der(forward_grad, prev_activations):
                     prev_result = prev_activations[0]
                     derived_acts = act_der(prev_result)
-                    return np.multiply(forward_grad, derived_acts)
+                    new_grad = np.multiply(forward_grad, derived_acts)
+                    return [new_grad]
 
                 return activation_der
 